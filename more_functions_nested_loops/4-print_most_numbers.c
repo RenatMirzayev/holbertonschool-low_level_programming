5630d780d5306c541f1489bbf3ff7e880fd4cb4b
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 11:24:15 +0000

    4numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,15 @@
  */
 void print_most_numbers(void)
 {
-    int i; /* Declare i before the loop */
-    for (i = 0; i <= 9; i++) /* Use a C89 style for loop */
-    {
-        if (i != 2 && i != 4) /* Skip 2 and 4 */
-        {
-            _putchar(i + '0'); /* Convert int to char */
-        }
-    }
-    _putchar('\n'); /* Print a new line at the end */
+	int i; /* Declare i before the loop */
+
+	for (i = 0; i <= 9; i++) /* Use a C89 style for loop */
+	{
+		if (i != 2 && i != 4) /* Skip 2 and 4 */
+		{
+			_putchar(i + '0'); /* Convert int to char */
+		}
+	}
+
+	_putchar('\n'); /* Print a new line at the end */
 }
