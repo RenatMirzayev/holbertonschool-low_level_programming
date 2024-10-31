63c9a3b0772107501611292d6f33d6cfcff174bf
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 13:01:38 +0000

    10numbers

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,33 @@
 #include "main.h"
 
+/**
+ * print_triangle - Prints a triangle of '#' characters.
+ * @size: The size of the triangle.
+ *
+ * Description: If size is 0 or less, the function prints only a new line.
+ * Otherwise, it prints a right-angled triangle of '#' characters
+ * with the specified size, aligned to the right.
+ */
 void print_triangle(int size)
 {
-	int row, space, hash;
+    int row, spaces;
 
-	if (size <= 0)
-	{
-		_putchar('\n'); /* Print a new line if size is 0 or less */
-		return;
-	}
+    if (size <= 0)
+    {
+        _putchar('\n'); /* Print a new line if size is 0 or less */
+        return;
+    }
 
-	for (row = 1; row <= size; row++)
-	{
-		for (space = size - row; space > 0; space--)
-		{
-			_putchar(' '); /* Print spaces */
-		}
-		for (hash = 1; hash <= row; hash++)
-		{
-			_putchar('#'); /* Print hashes */
-		}
-		_putchar('\n'); /* Move to the next line */
-	}
+    for (row = 1; row <= size; row++) /* Loop through each row */
+    {
+        for (spaces = size - row; spaces > 0; spaces--) /* Print spaces */
+        {
+            _putchar(' ');
+        }
+        for (int hashes = 1; hashes <= row; hashes++) /* Print hashes */
+        {
+            _putchar('#');
+        }
+        _putchar('\n'); /* Move to the next line */
+    }
 }
