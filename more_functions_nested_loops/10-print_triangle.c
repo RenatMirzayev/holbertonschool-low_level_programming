1dbfe29fc663cae6a52cc3e4d193d306330ce770
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 13:00:12 +0000

    10numbers

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,24 +2,24 @@
 
 void print_triangle(int size)
 {
-    int row, space, hash;
+	int row, space, hash;
 
-    if (size <= 0)
-    {
-        _putchar('\n'); /* Print a new line if size is 0 or less */
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n'); /* Print a new line if size is 0 or less */
+		return;
+	}
 
-    for (row = 1; row <= size; row++)
-    {
-        for (space = size - row; space > 0; space--)
-        {
-            _putchar(' '); /* Print spaces */
-        }
-        for (hash = 1; hash <= row; hash++)
-        {
-            _putchar('#'); /* Print hashes */
-        }
-        _putchar('\n'); /* Move to the next line */
-    }
+	for (row = 1; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+		{
+			_putchar(' '); /* Print spaces */
+		}
+		for (hash = 1; hash <= row; hash++)
+		{
+			_putchar('#'); /* Print hashes */
+		}
+		_putchar('\n'); /* Move to the next line */
+	}
 }
