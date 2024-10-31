02054c52367e352ce8bb3ee8f633bba2345fc4e3
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 12:13:22 +0000

    6numbers

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,18 @@
  */
 void print_line(int n)
 {
-    int i;  /* Declare the loop variable here */
+	int i;  /* Declare the loop variable here */
 
-    if (n <= 0)  /* If n is 0 or less */
-    {
-        _putchar('\n');  /* Print just a newline */
-    }
-    else
-    {
-        for (i = 0; i < n; i++)  /* Loop n times */
-        {
-            _putchar('_');  /* Print the '_' character */
-        }
-        _putchar('\n');  /* Print newline at the end */
-    }
+	if (n <= 0)  /* If n is 0 or less */
+	{
+		_putchar('\n');  /* Print just a newline */
+	}
+	else
+	{
+		for (i = 0; i < n; i++)  /* Loop n times */
+		{
+			_putchar('_');  /* Print the '_' character */
+		}
+		_putchar('\n');  /* Print newline at the end */
+	}
 }
