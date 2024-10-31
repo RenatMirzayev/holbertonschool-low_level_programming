d55c3219b19b0c495b429a3d061abe0a1a547612
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 12:30:58 +0000

    7numbers

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,22 @@
  */
 void print_diagonal(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n <= 0)  /* If n is 0 or less */
-    {
-        _putchar('\n');  /* Print just a newline */
-    }
-    else
-    {
-        for (i = 0; i < n; i++)  /* Loop n times */
-        {
-            for (j = 0; j < i; j++)
-            {
-                _putchar(' ');  /* Print spaces for the diagonal */
-            }
-            _putchar('\\');  /* Print the diagonal character */
-            _putchar('\n');  /* Print newline at the end of each line */
-        }
-    }
+	if (n <= 0)  /* If n is 0 or less */
+	{
+		_putchar('\n');  /* Print just a newline */
+	}
+	else
+	{
+		for (i = 0; i < n; i++)  /* Loop n times */
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');  /* Print spaces for the diagonal */
+			}
+			_putchar('\\');  /* Print the diagonal character */
+			_putchar('\n');  /* Print newline at the end of each line */
+		}
+	}
 }
