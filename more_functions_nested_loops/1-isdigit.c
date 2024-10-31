b3cf25f29f64299d9b377d5ce490eea0e83502ba
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 10:21:49 +0000

    1stdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,7 @@
-#include <stdio.h>
-
 int _isdigit(int c) {
     if (c >= '0' && c <= '9') {
         return 1;
     } else {
         return 0;
     }
-}
-
-int main() {
-    char c = '5';
-    if (_isdigit(c)) {
-        printf("%c — это цифра.\n", c);
-    } else {
-        printf("%c — это не цифра.\n", c);
-    }
-    return 0;
 }
