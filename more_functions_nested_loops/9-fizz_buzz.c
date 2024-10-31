2155054db7fbe64b4139bcaf483c183aee789c89
RenatMirzayev <renadmirzoyev@gmail.com>
2024-10-31 12:48:19 +0000

    9numbers

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,31 +11,31 @@
  */
 int main(void)
 {
-    int i; /* Declare the loop variable here */
+	int i; /* Declare the loop variable here */
 
-    for (i = 1; i <= 100; i++) /* Loop from 1 to 100 */
-    {
-        if (i % 3 == 0 && i % 5 == 0) /* Check for multiples of both 3 and 5 */
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0) /* Check for multiples of 3 */
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0) /* Check for multiples of 5 */
-        {
-            printf("Buzz");
-        }
-        else /* For all other numbers */
-        {
-            printf("%d", i);
-        }
-        if (i < 100) /* Add a space after each number/word except for the last one */
-        {
-            printf(" ");
-        }
-    }
-    printf("\n"); /* Print a new line at the end */
-    return (0);
+	for (i = 1; i <= 100; i++) /* Loop from 1 to 100 */
+	{
+		if (i % 3 == 0 && i % 5 == 0) /* Check for multiples of both 3 and 5 */
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0) /* Check for multiples of 3 */
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0) /* Check for multiples of 5 */
+		{
+			printf("Buzz");
+		}
+		else /* For all other numbers */
+		{
+			printf("%d", i);
+		}
+		if (i < 100) /* Add a space after each number/word except for the last one */
+		{
+			printf(" ");
+		}
+	}
+	printf("\n"); /* Print a new line at the end */
+	return (0);
 }
