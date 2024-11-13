735f51a922f037d3f3f41102b95dd18c7f82d699
RenatMirzayev <renadmirzoyev@gmail.com>
2024-11-13 14:58:13 +0000

    arrays4

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints chessboard
- * @a: pointer
+ * print_chessboard - prints the chessboard.
+ * @a: a 2D array representing the chessboard.
  */
-
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-
-	_putchar('\n');
-	}
+    for (i = 0; i < 8; i++)  // Loop through each row
+    {
+        for (j = 0; j < 8; j++)  // Loop through each column in the row
+        {
+            putchar(a[i][j]);  // Print the current character
+        }
+        putchar('\n');  // Print a new line after each row
+    }
 }
