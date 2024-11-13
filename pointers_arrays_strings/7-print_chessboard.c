7d797416493653783de459d1f762a2c488c04658
RenatMirzayev <renadmirzoyev@gmail.com>
2024-11-13 14:59:48 +0000

    arrays4

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,12 @@ void print_chessboard(char (*a)[8])
 {
     int i, j;
 
-    for (i = 0; i < 8; i++)  // Loop through each row
+    for (i = 0; i < 8; i++)  /* Loop through each row */
     {
-        for (j = 0; j < 8; j++)  // Loop through each column in the row
+        for (j = 0; j < 8; j++)  /* Loop through each column in the row */
         {
-            putchar(a[i][j]);  // Print the current character
+            putchar(a[i][j]);  /* Print the current character */
         }
-        putchar('\n');  // Print a new line after each row
+        putchar('\n');  /* Print a new line after each row */
     }
 }
